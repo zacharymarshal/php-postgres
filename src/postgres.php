749117781457942fb4e76928ef93be5c24932d26
Zachary Rankin <rankin.zachary@gmail.com>
2016-09-25 22:52:40 -0700

    Send startup message when connecting

--- src/postgres.php
+++ src/postgres.php
@@ -26,7 +26,67 @@ function connect($url, array $options = [])
         throw new \Exception("TODO: Could not connect");
     }
 
+    sendStartupMessage($conn, $db_opts['user'], $db_opts['pass'], ltrim($db_opts['path'], '/'));
+
     return $conn;
+}
+
+/**
+ * @param $conn
+ * @param string $user
+ * @param $password
+ * @param string $database
+ * @throws \Exception
+ */
+function sendStartupMessage($conn, $user, $password, $database)
+{
+    send($conn, sprintf(
+        '3::int16 0::int16 "user\0%s\0database\0%s\0"::string',
+        $user,
+        $database
+    ));
+
+    while (true) {
+        list($msg, $msg_code) = get($conn);
+        if ($msg_code === 'R') {
+            authenticate($conn, $msg, $password);
+        }
+        if ($msg_code === 'E') {
+            throw new \Exception("TODO: Error! " . $msg);
+        }
+        if ($msg_code === 'Z') {
+            // TODO: Set some sort of state letting us know that
+            // we are ready for a query
+            return;
+        }
+    }
+}
+
+/**
+ * @param $conn
+ * @param $binary_auth_code
+ * @param $password
+ * @throws \Exception
+ */
+function authenticate($conn, $binary_auth_code, $password)
+{
+    $auth_code = int32($binary_auth_code);
+    if ($auth_code === 0) {
+        return;
+    }
+
+    if ($auth_code === 3) {
+        send($conn, sprintf('"%s"::string', $password), 'p');
+        list($binary_auth_code, $msg_code) = get($conn);
+        if ($msg_code != 'R') {
+            throw new \Exception("TODO: Unexpected response code " . $msg_code);
+        }
+        $auth_code = int32($binary_auth_code);
+        if ($auth_code !== 0) {
+            throw new \Exception("TODO: Unexpected auth code " . $auth_code);
+        }
+        return;
+    }
 }
 
 /**
@@ -54,31 +114,22 @@ function send($conn, $msg, $msg_code = '')
 
 /**
  * @param $conn
+ * @return array
  * @throws \Exception
  */
-function get($conn, callable $callback)
+function get($conn)
 {
-    $read = $write = $except = [];
-    $read[] = $conn;
-    $can_read = socket_select($read, $write, $except, 1);
-    if ($can_read === 0) {
-        return;
+    $msg_code_result = socket_recv($conn, $bin_msg_code, 5, MSG_WAITALL);
+    if ($msg_code_result === false) {
+        throw new \Exception("TODO: Error getting message code");
+    }
+    $msg_code = unpack('a1code/N1length', $bin_msg_code);
+    $get_msg_result = socket_recv($conn, $msg, $msg_code['length'] - 4, 0);
+    if ($get_msg_result === false) {
+        throw new \Exception("TODO: Error getting message data");
     }
 
-    while (($r = socket_recv($conn, $buf, 5, 0))) {
-        $unpacked_buf = unpack('a1msg_code/N1msg_length', $buf);
-        $get_msg_result = socket_recv($conn, $msg, $unpacked_buf['msg_length'] - 4, 0);
-        if ($get_msg_result === false) {
-            throw new \Exception("TODO: Error getting message data");
-        }
-
-        call_user_func($callback, $unpacked_buf['msg_code'], $msg);
-    }
-
-    if ($r === false) {
-        var_dump(socket_strerror(socket_last_error()));
-        exit;
-    }
+    return [$msg, $msg_code['code']];
 }
 
 /**
@@ -170,4 +221,22 @@ function getMessageToken($msg)
         'type'  => 'unknown',
         'value' => $msg,
     ];
+}
+
+/**
+ * @param $str
+ * @return string
+ * @throws \Exception
+ */
+function int32(&$str)
+{
+    $binary_int = substr($str, 0, 4);
+    if (!$binary_int) {
+        throw new \Exception("TODO: Not enough bytes");
+    }
+    $str = substr($str, 4);
+
+    $int32 = current(unpack('N', $binary_int));
+
+    return $int32;
 }

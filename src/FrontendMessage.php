c1ece0d12c3a6281469d16affcc7bb40bbc2391d
Zachary Rankin <rankin.zachary@gmail.com>
2016-09-28 20:59:42 -0700

    Abstract creating a front-end message

---
+++ src/FrontendMessage.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Postgres;
+
+/**
+ * Represents a string to be sent to the postgres backend.
+ *
+ * @package Postgres
+ */
+class FrontendMessage
+{
+    /**
+     * @var string
+     */
+    private $msg = "";
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        $this->writeNUL();
+
+        return $this->msg;
+    }
+
+    /**
+     * @param string $ident
+     */
+    public function writeIdent($ident)
+    {
+        $this->msg .= $ident;
+    }
+
+    /**
+     * Write a null character to the message
+     */
+    public function writeNUL()
+    {
+        $this->msg .= "\0";
+    }
+
+    /**
+     * @param int $int
+     */
+    public function writeInt32($int)
+    {
+        $this->msg .=  pack('N', $int);
+    }
+
+    /**
+     * @param int $int
+     */
+    public function writeInt16($int)
+    {
+        $this->msg .= pack('n', $int);
+    }
+
+    /**
+     * @param $str
+     */
+    public function writeString($str)
+    {
+        $this->msg .= $str;
+    }
+}
